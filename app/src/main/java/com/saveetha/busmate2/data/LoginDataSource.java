b8297a6cc0af0f470a4b1d5cd2899086706e368a
vignesh <vigneshmr13@gmail.com>
2020-02-04 18:16:55 +0530

    BusMate under progress - Vicky 5:58 am - 3/3/20

--- app/src/main/java/com/saveetha/busmate2/data/LoginDataSource.java
+++ app/src/main/java/com/saveetha/busmate2/data/LoginDataSource.java
@@ -17,6 +17,7 @@ public class LoginDataSource {
                     new LoggedInUser(
                             java.util.UUID.randomUUID().toString(),
                             "Jane Doe");
+
             return new Result.Success<>(fakeUser);
         } catch (Exception e) {
             return new Result.Error(new IOException("Error logging in", e));
